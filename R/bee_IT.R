25836f58ddb05e531cfcdc798c09de2afe2f4e62
liamkendall <lkendal2@myune.edu.au>
2018-08-19 14:20:09 +1000

    data package

---
+++ R/bee_IT.R
@@ -0,0 +1,3 @@
+#' A BRMS model fit estimating the dry weight (mg) by intertegular distance (mm) for ~400 bee species.
+#'
+"bee_IT"
---
+++ R/bee_tax_mod.R
@@ -0,0 +1,3 @@
+#' A BRMS model fit estimating the dry weight (mg) by intertegular distance (mm), sex and taxonomic family for ~400 bee species.
+#'
+"bee_tax_mod"
