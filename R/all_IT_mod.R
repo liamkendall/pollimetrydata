cb5192b0bcab33e61ed9e22a36ec87981c8b507f
Liam Kendall <liamkendall@Liams-iMac.local>
2022-02-07 22:05:51 +0100

    foraging range models :)

---
+++ R/all_IT_mod.R
@@ -0,0 +1,3 @@
+#' A BRMS model fit estimating bee foraging range s(m) by intertegular distance (mm), easurement type and phylogeny 
+#'
+"all_IT_mod"
---
+++ R/all_social_mod.R
@@ -0,0 +1,3 @@
+#' A BRMS model fit estimating bee foraging ranges (m) by intertegular distance (mm), sociality, measurement type and phylogeny. 
+#'
+"all_social_mod"
