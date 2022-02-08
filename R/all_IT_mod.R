04f9d9c122b67682a2e4f48f5645ae3336ce9abf
liamkendall <lkendal2@myune.edu.au>
2022-02-08 08:59:46 +0100

    spellcheck

--- R/all_IT_mod.R
+++ R/all_IT_mod.R
@@ -1,3 +1,3 @@
-#' A BRMS model fit estimating bee foraging range s(m) by intertegular distance (mm), easurement type and phylogeny 
+#' A BRMS model fit estimating bee foraging ranges (m) by intertegular distance (mm), easurement type and phylogeny 
 #'
 "all_IT_mod"
